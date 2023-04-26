c530ce387582286d795e803c16b2606ea6e4bdd8
Thies Oelke <thies.oelke@gmail.com>
2023-04-26 13:06:54 +0200

    - connects ProgramBrowser with the CMS API - renders program cards in ProgramBrowser based on the query with different styling

--- src/ProgramBrowser/ProgramBrowser.component.jsx
+++ src/ProgramBrowser/ProgramBrowser.component.jsx
@@ -3,36 +3,46 @@ import { Navbar } from "../Navbar";
 import { usePrograms } from "../hooks/usePrograms";
 
 function ProgramBrowser() {
-  const { error, data, loading } = usePrograms()
+  const { error, data, loading } = usePrograms();
 
-  console.log({error, data, loading});
+  if (loading)
+    return (
+      <div className="min-h-screen bg-app-dark px-4 py-10 pb-16">
+        <h2 className="headline-2">Browse</h2>
+        <p className="mtext">Loading...</p>
 
-  if (loading) return (
-    <div className="min-h-screen bg-app-dark px-4 py-10 pb-16">
-      <h2 className="headline-2">Browse</h2>
-      <p className="mtext">Loading...</p>
-      <Navbar nav="programs" />
-    </div>
-  )
+        <Navbar nav="programs" />
+      </div>
+    );
 
-  if (error) return (
-    <div className="min-h-screen bg-app-dark px-4 py-10 pb-16">
-      <h2 className="headline-2">Browse</h2>
-      <p className="mtext">Es ist ein Fehler aufgetreten</p>
-      <p className="stext">{error}</p>
-      <Navbar nav="programs" />
-    </div>
-  )
+  if (error)
+    return (
+      <div className="min-h-screen bg-app-dark px-4 py-10 pb-16">
+        <h2 className="headline-2">Browse</h2>
+        <p className="mtext">Es ist ein Fehler aufgetreten</p>
+        <p className="stext">{error}</p>
+
+        <Navbar nav="programs" />
+      </div>
+    );
 
   return (
     <div className="min-h-screen bg-app-dark px-4 py-10 pb-16">
       <h2 className="headline-2">Browse</h2>
-      <ActionCard type="program" color="gradient-1" height="214px">TestCard</ActionCard>
-      <ActionCard type="program" color="gradient-2" height="214px" />
-      <ActionCard type="program" color="gradient-3" height="214px" />
-      <ActionCard type="program" color="gradient-1" height="214px" />
-      <ActionCard type="program" color="gradient-2" height="214px" />
-      <ActionCard type="program" color="gradient-3" height="214px" />
+      {data.programs.map(({id, name}, index) => {
+        // In order to check if gradient-<1, 2, 3> to apply the following calculation is used
+        // (index + 1) / 3 --> returns x.3333 for first, x.6666 for second, x.0000 for third
+        // result % 1 to only return the digits after the .
+        // result.toFixed(1) returns a string being either '0.0' for third, '0.7' for second, '0.3 for first
+        if ((((index + 1) / 3) % 1).toFixed(1) === '0.0') {
+        return <ActionCard key={id} type="program" color="gradient-3">{name}</ActionCard>
+        } else if ((((index + 1) / 3) % 1).toFixed(1) === '0.7') {
+          return <ActionCard key={id} type="program" color="gradient-2">{name}</ActionCard>
+        } else {
+          return <ActionCard key={id} type="program" color="gradient-1">{name}</ActionCard>
+        }
+      }
+      )}
 
       <Navbar nav="programs" />
     </div>
