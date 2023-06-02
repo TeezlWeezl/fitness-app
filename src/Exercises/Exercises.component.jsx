543cc415516c3455367eeb27b2d188de4642ebe0
Thies Oelke <thies.oelke@gmail.com>
2023-06-02 16:41:09 +0200

    refactoring of Exercises component into separate components

--- src/Exercises/Exercises.component.jsx
+++ src/Exercises/Exercises.component.jsx
@@ -1,250 +1,20 @@
-import { Link, useParams } from "react-router-dom";
-import { useExercises } from "../hooks/useExercises";
 import {
-  CarouselProvider,
-  Slider,
-  Slide,
   ButtonBack,
   ButtonNext,
+  CarouselProvider,
+  Slider
 } from "pure-react-carousel";
-import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import { useEffect, useRef, useState } from "react";
-import { ActionButton } from "../ActionButton";
+import { Link, useParams } from "react-router-dom";
+import { useExercises } from "../hooks/useExercises";
+import { ProgressCircle } from "./ProgressCircle";
+import { SlideContent } from "./SlideContent";
 
-import closeIcon from "../icon/close.svg";
-import prev from "../icon/Exercises__slider-prev.svg";
-import next from "../icon/Exercises__slider-next.svg";
-import info from "../icon/Exercises__info.svg";
-import bell3x from "../audio/bell-3x.mp3";
-import bell1x from "../audio/bell-1x.mp3";
-import circle from "../icon/Exercises__circle_grey.svg";
 import "pure-react-carousel/dist/react-carousel.es.css";
+import next from "../icon/Exercises__slider-next.svg";
+import prev from "../icon/Exercises__slider-prev.svg";
+import closeIcon from "../icon/close.svg";
 import "./Exercises.style.css";
-
-const renderTime = (
-  { remainingTime },
-  startTime,
-  exerciseDuration,
-  type,
-  bellsActive
-) => {
-  if (remainingTime === startTime) {
-    return (
-      <div>
-        <div className="mtext">Klicken</div>
-        <div className="mtext">zum</div>
-        <div className="mtext">starten</div>
-      </div>
-    );
-  } else if (remainingTime <= startTime && remainingTime > exerciseDuration) {
-    return (
-      <div>
-        <div className="mtext">Get</div>
-        <div className="mtext text-3xl">{remainingTime - exerciseDuration}</div>
-        <div className="mtext">Ready</div>
-      </div>
-    );
-  } else if (remainingTime === exerciseDuration) {
-    // Ring the starting bell once for exercises
-    if (type === "duration") {
-      if (!bellsActive.current) {
-        new Audio(bell1x).play();
-        bellsActive.current = true;
-      }
-      return (
-        <div>
-          <div className="mtext text-3xl">Go!</div>
-        </div>
-      );
-    }
-    return (
-      <div>
-        <div className="mtext">Get</div>
-        <div className="mtext text-3xl">{remainingTime - exerciseDuration}</div>
-        <div className="mtext">Ready</div>
-      </div>
-    );
-  } else if (remainingTime < exerciseDuration && remainingTime > 0) {
-    return (
-      <div>
-        <div className="mtext">Verbleibend</div>
-        <div className="mtext text-3xl">{remainingTime}</div>
-        <div className="mtext">Sekunden</div>
-      </div>
-    );
-  } else {
-    // Ring the bell three times for exercises
-    if (type === "duration" && bellsActive.current) {
-      new Audio(bell3x).play();
-      bellsActive.current = false;
-    }
-    // Reset the helper variable that next exercise can start
-    return <div className="mtext text-3xl">Fertig</div>;
-  }
-};
-
-const renderSlide = ({
-  index,
-  id,
-  type,
-  name,
-  duration,
-  reps,
-  description,
-  isPlaying,
-  setIsPlaying,
-  slideButtons,
-  bellsActive,
-  timerActive,
-  setExercisesFinished,
-}) => {
-  let exerciseContainer;
-
-  if (type === "duration" || type === "break") {
-    exerciseContainer = (
-      <div className="min-h-full">
-        <button
-          onClick={() => {
-            // The function calls below are necessary to prevent that multiple timers are running at the same time
-
-            // if the timer is not active, toggle the timer
-            if (!timerActive.current) {
-              setIsPlaying((prev) => {
-                const newState = [...prev];
-                newState[index] = !newState[index];
-                return newState;
-              });
-              timerActive.current = true;
-              return;
-            }
-            // if the timer is active, toggle the timer only of the exercise that is currently running
-            if (timerActive.current && isPlaying[index]) {
-              setIsPlaying((prev) => {
-                const newState = [...prev];
-                newState[index] = !newState[index];
-                return newState;
-              });
-              timerActive.current = false;
-            }
-          }}
-          className="headline-1 absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]"
-        >
-          <CountdownCircleTimer
-            trailColor="#4D5059"
-            trailStrokeWidth={18}
-            strokeWidth={18}
-            isPlaying={isPlaying[index]}
-            duration={duration}
-            colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
-            colorsTime={[duration, (duration * 2) / 3, (duration * 1) / 3, 0]}
-            onComplete={() => {
-              timerActive.current = false;
-              setExercisesFinished((prev) => {
-                prev[index] = true;
-                return prev;
-              });
-              return { shouldRepeat: false };
-            }}
-            initialRemainingTime={type !== "break" && duration + 6}
-          >
-            {({ remainingTime }) =>
-              renderTime(
-                { remainingTime },
-                type === "break" ? duration : duration + 6,
-                duration,
-                type,
-                bellsActive
-              )
-            }
-          </CountdownCircleTimer>
-        </button>
-        <h1 className="headline-1 absolute bottom-[30%] left-[50%] translate-x-[-50%] text-center">
-          {name}
-        </h1>
-      </div>
-    );
-  }
-
-  if (type === "reps") {
-    exerciseContainer = (
-      <div className="min-h-full">
-        <h1 className="headline-1 absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]">
-          {reps} x
-        </h1>
-        <h1 className="headline-1 absolute bottom-[30%] left-[50%] translate-x-[-50%] text-center">
-          {name}
-        </h1>
-      </div>
-    );
-  }
-
-  return (
-    <Slide index={index} key={id}>
-      {exerciseContainer}
-      {
-        /* Show a description info box if there is one */
-        description && (
-          <div
-            id="info-box"
-            className="absolute left-0 top-[90%] h-[80%] w-full rounded-t-[30px] bg-app-medium transition-all"
-          >
-            <div></div>
-            <div
-              className="min-h-full min-w-full"
-              onClick={(e) => {
-                slideButtons.current.classList.add("hidden");
-                e.currentTarget.parentElement.classList.remove("top-[90%]");
-                e.currentTarget.parentElement.classList.add("top-[20%]");
-              }}
-            >
-              <div className="absolute right-4 top-4 flex min-h-[35px] min-w-[35px] justify-center rounded-full bg-app-dark">
-                <img src={info} alt="exercise info" />
-              </div>
-              <div className="absolute top-[15%] max-h-[70%] overflow-scroll px-9 text-left">
-                <h1 className="headline-1">{name}</h1>
-                <p className="mtext mt-5">{description}</p>
-              </div>
-            </div>
-            <ActionButton
-              className="bottom absolute top-[87%] text-white"
-              color="bg-app-dark"
-              onClick={(e) => {
-                slideButtons.current.classList.remove("hidden");
-                e.currentTarget.parentElement.classList.remove("top-[20%]");
-                e.currentTarget.parentElement.classList.add("top-[90%]");
-                e.currentTarget.previousElementSibling.children[1].scrollTop = 0;
-              }}
-            >
-              ok!
-            </ActionButton>
-          </div>
-        )
-      }
-    </Slide>
-  );
-};
-
-const ProgressCircle = ({ index, currentSlide, color, exercisesFinished }) => {
-  if (currentSlide === index) {
-    /* Only show colored circle, if it is the current slide */
-    return (
-      <div
-        className={`min-h-[25px] min-w-[25px] -translate-x-[20px] rounded-full border-none ${color}`}
-      ></div>
-    );
-  } else if (exercisesFinished[index]) {
-    return (
-      <div
-        className={`min-h-[25px] min-w-[25px] -translate-x-[20px] rounded-full border-none bg-green-500`}
-      ></div>
-    );
-  } else
-    return (
-      <div
-        className={`min-h-[25px] min-w-[25px] -translate-x-[20px] rounded-full border-[5px] border-app-medium`}
-      ></div>
-    );
-};
 
 function Exercises(props) {
   // extract the params from the url
@@ -342,22 +112,25 @@ function Exercises(props) {
                 { id, duration, reps, exercise: { type, name, description } },
                 index
               ) => {
-                return renderSlide({
-                  index,
-                  id,
-                  type,
-                  name,
-                  duration,
-                  description,
-                  reps,
-                  isPlaying,
-                  setIsPlaying,
-                  slideButtons,
-                  bellsActive,
-                  timerActive,
-                  exercisesFinished,
-                  setExercisesFinished,
-                });
+                return (
+                  <SlideContent
+                    key={id}
+                    index={index}
+                    id={id}
+                    type={type}
+                    name={name}
+                    duration={duration}
+                    description={description}
+                    reps={reps}
+                    isPlaying={isPlaying}
+                    setIsPlaying={setIsPlaying}
+                    slideButtons={slideButtons}
+                    bellsActive={bellsActive}
+                    timerActive={timerActive}
+                    exercisesFinished={exercisesFinished}
+                    setExercisesFinished={setExercisesFinished}
+                  />
+                );
               }
             )}
           </Slider>
---
+++ src/Exercises/SlideContent/SlideContent.component.jsx
@@ -0,0 +1,212 @@
+import { Slide } from "pure-react-carousel";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+import { ActionButton } from '../../ActionButton';
+
+import bell1x from "../../audio/bell-1x.mp3";
+import bell3x from "../../audio/bell-3x.mp3";
+import info from "../../icon/Exercises__info.svg";
+
+const renderTime = (
+  { remainingTime },
+  startTime,
+  exerciseDuration,
+  type,
+  bellsActive
+) => {
+  if (remainingTime === startTime) {
+    return (
+      <div>
+        <div className="mtext">Klicken</div>
+        <div className="mtext">zum</div>
+        <div className="mtext">starten</div>
+      </div>
+    );
+  } else if (remainingTime <= startTime && remainingTime > exerciseDuration) {
+    return (
+      <div>
+        <div className="mtext">Get</div>
+        <div className="mtext text-3xl">{remainingTime - exerciseDuration}</div>
+        <div className="mtext">Ready</div>
+      </div>
+    );
+  } else if (remainingTime === exerciseDuration) {
+    // Ring the starting bell once for exercises
+    if (type === "duration") {
+      if (!bellsActive.current) {
+        new Audio(bell1x).play();
+        bellsActive.current = true;
+      }
+      return (
+        <div>
+          <div className="mtext text-3xl">Go!</div>
+        </div>
+      );
+    }
+    return (
+      <div>
+        <div className="mtext">Get</div>
+        <div className="mtext text-3xl">{remainingTime - exerciseDuration}</div>
+        <div className="mtext">Ready</div>
+      </div>
+    );
+  } else if (remainingTime < exerciseDuration && remainingTime > 0) {
+    return (
+      <div>
+        <div className="mtext">Verbleibend</div>
+        <div className="mtext text-3xl">{remainingTime}</div>
+        <div className="mtext">Sekunden</div>
+      </div>
+    );
+  } else {
+    // Ring the bell three times for exercises
+    if (type === "duration" && bellsActive.current) {
+      new Audio(bell3x).play();
+      bellsActive.current = false;
+    }
+    // Reset the helper variable that next exercise can start
+    return <div className="mtext text-3xl">Fertig</div>;
+  }
+};
+
+const SlideContent = ({
+  index,
+  id,
+  type,
+  name,
+  duration,
+  reps,
+  description,
+  isPlaying,
+  setIsPlaying,
+  slideButtons,
+  bellsActive,
+  timerActive,
+  setExercisesFinished,
+}) => {
+  let exerciseContainer;
+
+  if (type === "duration" || type === "break") {
+    exerciseContainer = (
+      <div className="min-h-full">
+        <button
+          onClick={() => {
+            // The function calls below are necessary to prevent that multiple timers are running at the same time
+
+            // if the timer is not active, toggle the timer
+            if (!timerActive.current) {
+              setIsPlaying((prev) => {
+                const newState = [...prev];
+                newState[index] = !newState[index];
+                return newState;
+              });
+              timerActive.current = true;
+              return;
+            }
+            // if the timer is active, toggle the timer only of the exercise that is currently running
+            if (timerActive.current && isPlaying[index]) {
+              setIsPlaying((prev) => {
+                const newState = [...prev];
+                newState[index] = !newState[index];
+                return newState;
+              });
+              timerActive.current = false;
+            }
+          }}
+          className="headline-1 absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]"
+        >
+          <CountdownCircleTimer
+            trailColor="#4D5059"
+            trailStrokeWidth={18}
+            strokeWidth={18}
+            isPlaying={isPlaying[index]}
+            duration={duration}
+            colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
+            colorsTime={[duration, (duration * 2) / 3, (duration * 1) / 3, 0]}
+            onComplete={() => {
+              timerActive.current = false;
+              setExercisesFinished((prev) => {
+                prev[index] = true;
+                return prev;
+              });
+              return { shouldRepeat: false };
+            }}
+            initialRemainingTime={type !== "break" && duration + 6}
+          >
+            {({ remainingTime }) =>
+              renderTime(
+                { remainingTime },
+                type === "break" ? duration : duration + 6,
+                duration,
+                type,
+                bellsActive
+              )
+            }
+          </CountdownCircleTimer>
+        </button>
+        <h1 className="headline-1 absolute bottom-[30%] left-[50%] translate-x-[-50%] text-center">
+          {name}
+        </h1>
+      </div>
+    );
+  }
+
+  if (type === "reps") {
+    exerciseContainer = (
+      <div className="min-h-full">
+        <h1 className="headline-1 absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]">
+          {reps} x
+        </h1>
+        <h1 className="headline-1 absolute bottom-[30%] left-[50%] translate-x-[-50%] text-center">
+          {name}
+        </h1>
+      </div>
+    );
+  }
+
+  return (
+    <Slide index={index} key={id}>
+      {exerciseContainer}
+      {
+        /* Show a description info box if there is one */
+        description && (
+          <div
+            id="info-box"
+            className="absolute left-0 top-[90%] h-[80%] w-full rounded-t-[30px] bg-app-medium transition-all"
+          >
+            <div></div>
+            <div
+              className="min-h-full min-w-full"
+              onClick={(e) => {
+                slideButtons.current.classList.add("hidden");
+                e.currentTarget.parentElement.classList.remove("top-[90%]");
+                e.currentTarget.parentElement.classList.add("top-[20%]");
+              }}
+            >
+              <div className="absolute right-4 top-4 flex min-h-[35px] min-w-[35px] justify-center rounded-full bg-app-dark">
+                <img src={info} alt="exercise info" />
+              </div>
+              <div className="absolute top-[15%] max-h-[70%] overflow-scroll px-9 text-left">
+                <h1 className="headline-1">{name}</h1>
+                <p className="mtext mt-5">{description}</p>
+              </div>
+            </div>
+            <ActionButton
+              className="bottom absolute top-[87%] text-white"
+              color="bg-app-dark"
+              onClick={(e) => {
+                slideButtons.current.classList.remove("hidden");
+                e.currentTarget.parentElement.classList.remove("top-[20%]");
+                e.currentTarget.parentElement.classList.add("top-[90%]");
+                e.currentTarget.previousElementSibling.children[1].scrollTop = 0;
+              }}
+            >
+              ok!
+            </ActionButton>
+          </div>
+        )
+      }
+    </Slide>
+  );
+};
+
+export { SlideContent };
